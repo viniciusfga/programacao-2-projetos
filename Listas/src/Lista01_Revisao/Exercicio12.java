b0d74c896269ff1937c082f27839d3e116df0185
Vinicius F.G. Araújo <viniciusfg.araujo@gmail.com>
2025-08-24 16:50:53 -0300

    - Add solução exercicios 12, 13 e 14 lista 1

--- Listas/src/Lista01_Revisao/Exercicio12.java
+++ Listas/src/Lista01_Revisao/Exercicio12.java
@@ -1,11 +1,28 @@
 package Lista01_Revisao;
 
+import java.util.Scanner;
+
 public class Exercicio12 {
     public static void main(String[] args) {
 
-        System.out.println("");
+        Scanner sc = new Scanner(System.in);
 
-        
+        System.out.println("Calcular fatorial de um número informado.");
+        int n = sc.nextInt();
 
+        if (n < 0) {
+            System.out.println("Fatorial não é definido para números negativos.");
+            sc.close();
+            return;
+        }
+
+        long fat = 1;
+        for (int i = n; i > 0; i--) {
+            fat *= i;
+        }
+
+        System.out.println("O fatorial de " + n + " é " + fat);
+
+        sc.close();;
     }
 }
