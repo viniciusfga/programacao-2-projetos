91fa8b4b5da92f07b7e23281a76e7ed31ad3d6bf
viniciusfga <viniciusfg.araujo@gmail.com>
2025-09-18 16:48:14 -0300

    - Add Exercicios da lista

--- Listas/src/Lista03/Exercicio07.java
+++ Listas/src/Lista03/Exercicio07.java
@@ -1,12 +1,33 @@
 package Lista03;
-
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exercicio07 {
+    /*
+    Remova palavras duplicadas consecutivas em um texto (ex.: "oi oi tudo bem" -> "oi tudo bem")
+     */
+
+    static String removerDuplicados(String str) {
+        String resultado = "";
+
+        String[] palavras = str.split(" ");
+        for (int i = 0; i < palavras.length; i++) {
+            for (int j = i + 1; j < palavras.length; j++) {
+                if (palavras[i].equals(palavras[j])) {
+                    palavras[j]  = "";
+                }
+            }
+            resultado += palavras[i] + " ";
+        }
+
+        return resultado.strip().replaceAll("\\s+", " ");
+    }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String texto;
 
-
+        texto = "oi oi tudo bem"; //sc.nextLine();
+        String resultado = removerDuplicados(texto);
+        System.out.println(resultado);
 
     }
 }
--- Listas/src/Lista03/Exercicio08.java
+++ Listas/src/Lista03/Exercicio08.java
@@ -1,11 +1,81 @@
 package Lista03;
 
-import java.util.Scanner;
-
 public class Exercicio08 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+    /*
+    Faça um programa que use laços de repetição para imprimir a música de Walter Franco, Mame D’Agua.
+    Yeah eu 2
+    Yeah eu te amo 2
+    Yeah eu te amo muito 1
+    Yeah eu te amo muito mas 1
+    Yeah eu te amo muito mas agora 1
+    Yeah eu te amo muito mas agora é tarde 2
+    Yeah eu te amo muito mas agora é tarde eu vou 2
+    Yeah eu te amo muito mas agora é tarde eu vou dormir 1
+    Yeah eu te amo muito mas agora é tarde eu vou 2
+    Yeah eu te amo muito mas agora é tarde 2
+    Yeah eu te amo muito mas agora 1
+    Yeah eu te amo muito mas 1
+    Yeah eu te amo muito 1
+    Yeah eu te amo 2
+    Yeah eu 2
+    2 - 2 - 1 - 1 - 1 - 2 - 2 - 1
+    (0 - 1) (2 - 3) - 4 - 5 - 6 (7 - 8) (9 - 10) + 11
 
+    */
+    static void MameDAgua(String texto) {
+        String[] musica = texto.split(" ");
+
+        System.out.println(musica.length);
+        System.out.println();
+
+        int contador = 0;
+
+        for (int i = 0; i < 8; i++) {
+            if (contador == 0) {
+                contador = 2;
+            }
+
+            for (int j = 0; j < contador; j++) {
+                System.out.print(musica[j] + " ");
+            }
+            if (contador == 2)
+                contador = 3;
+            if (contador == 7)
+                contador = 8;
+            if (contador == 9)
+                contador = 10;
+
+            contador++;
+            System.out.println();
+        }
+
+        contador = 11;
+
+        for (int i = 0; i < 7; i++) {
+
+            if (contador == 3)
+                contador = 2;
+
+            for (int j = 0; j < contador; j++) {
+                System.out.print(musica[j] + " ");
+            }
+
+            if (contador == 11)
+                contador = 10;
+            if (contador == 9)
+                contador = 8;
+
+            contador--;
+            System.out.println();
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+        String musica = "Yeah eu te amo muito mas agora é tarde eu vou dormir";
+        MameDAgua(musica);
 
     }
 }
